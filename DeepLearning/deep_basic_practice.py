918029c90e3de2dd2101de92b58b36412834d14b
Leegihong <dlrlghd96@hanmail.net>
2020-09-09 11:40:17 +0900

    learn about gradient descent:

--- DeepLearning/deep_basic_practice.py
+++ DeepLearning/deep_basic_practice.py
@@ -38,3 +38,38 @@ def cross_entropy_error(y, t):
 # 신경망 학습에서 지료플 정확도를 삼는다 => 매개변수의 사소한 변화에 거의 반응을 보이지 않음 // 반응이 있더라도 불연속적으로 갑자기 변화
 # 이는 활성화 함수로 계단 함수보다 예를 들어 시그모이드 함수를 사용하는 이유와 비슷 -> 시그모이드 함수의 미분은 어느 장소라도 0이 되지 않음
 
+# 모든 변수의 편미분을 벡터로 정리한 것 -> gradient
+def numerical_gradient(f, x): # f는 편미분하고자 하는 함수 // x는 어느 점에서 알고싶은 값들의 list ex) [3.0, 4.0]
+    h = 1e-4
+    grad = np.zeros_like(x) # x와 형상이 같은 배열을 생성
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        #f(x+h) 계산
+        x[idx] = tmp_val +h
+        fxh1 = f(x)
+
+        #f(x-h) 계산
+        x[idx] = tmp_val -h
+        fxh2 = f(x)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val
+    return grad
+
+#gradient descent
+def gradient_descent(f, init_X, lr = 0.01, step_num = 100): # f : 최적화하려는 함수, init_x : 초기값, step_num : 경사법에 따른 반복 횟수
+    x = init_X
+    
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+    return x
+
+print("-------------------------------------------------------")
+#ex)
+def funtion_2(x):
+    return x[0] ** 2 + x[1] ** 2
+
+init_X = np.array([-3.0, 4.0])
+print(gradient_descent(funtion_2, init_X = init_X, lr = 0.1, step_num = 100))
+
